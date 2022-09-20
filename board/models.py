efca56d3a54d749dce71878b8741656c9f7cd7bd
natia <natia.tsibadze@gmail.com>
2022-09-20 22:29:28 +0400

    add fields to model migrate

--- board/models.py
+++ board/models.py
@@ -1,11 +1,15 @@
 from distutils import text_file
 from pyexpat import model
 from django.db import models
+from account.models import UserProfile
 
 # Create your models here.
 class Posts(models.Model):
+    title = models.CharField(max_length=255)
     text = models.TextField()
-    auther = models.CharField(max_length=50)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    posted_date = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
-        return self.text[:15]
+        return self.title
