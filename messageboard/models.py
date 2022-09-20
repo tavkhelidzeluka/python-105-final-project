8bb996acad4c943288529958f99af2a565e34056
natia <natia.tsibadze@gmail.com>
2022-09-20 17:21:47 +0400

    add model massages

--- messageboard/models.py
+++ messageboard/models.py
@@ -1,3 +1,8 @@
+from distutils import text_file
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class Messages(models.Model):
+    text: models.TextField(max_length=500)
+    auther: models.TextField(max_length=50)
